89031ae2cb10978350da9627c2b0aef51196146e
OnDarlingMC <98362972+OnDarlingMC@users.noreply.github.com>
2022-08-07 22:51:17 -0400

    Update staff-updates.js

--- commands/onlystaff/staff-updates.js
+++ commands/onlystaff/staff-updates.js
@@ -1,14 +1,45 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { CommandInteraction, MessageEmbed } = require("discord.js");
 
-const data = new SlashCommandBuilder()
-	.setName('gif')
-	.setDescription('Sends a random gif!')
-	.addStringOption(option =>
-		option.setName('category')
-			.setDescription('The gif category')
-			.setRequired(true)
-			.addChoices(
-				{ name: 'Funny', value: 'gif_funny' },
-				{ name: 'Meme', value: 'gif_meme' },
-				{ name: 'Movie', value: 'gif_movie' },
-			));
+
+module.exports = {
+	name: "afk",
+	description: "user afk on ticket",
+	type: 'CHAT_INPUT',
+	options: [
+		{
+			name: "user",
+			description: "The user to add to saying afk",
+			type: "USER",
+			required: true
+		},
+		{
+			name: "choice",
+			description: "The user to add to saying afk",
+			type: "boolean",
+			required: true
+		},
+		{
+			name: "role",
+			description: "The user to add to saying afk",
+			type: "mentionable",
+			required: false
+		}
+	],
+	/**
+	 *
+	 * @param {import("../..").Bot} client
+	 * @param {CommandInteraction} interaction
+	 * @param {String[]} args
+   */
+	run: async (client, interaction, args) => {
+		const user = interaction.options.getUser('user');
+		const boolean = interaction.options.getBoolean('choice');
+		const mentionable = interaction.options.getMentionable('mentionable');
+		.AddChoice("Joined", joined)
+		.AddChoice("Promote", promoted)
+            	.AddChoice("Demote", demoted)
+            	.AddChoice("Resign", resigned)
+            	.AddChoice("Blacklisted", blacklisted)
+    interaction.reply({ content: `${interaction.options.getUser('user')} has been to ${interaction.options.getUser('choices')} to ${interaction.options.getUser('role')}`})
+	},
+};
